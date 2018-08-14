2bb6775336ca0901d499f2f9fe2f04694f63ec06
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2018-08-15 00:23:46 +0100

    Initial commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: ['./src/client/main.js'],
+  },
+  mode: 'development',
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: 'dist',
+    overlay: true,
+    stats: {
+      colors: true,
+    },
+    hot: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'imgs/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {},
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
---
+++ config/webpack.prod.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: ['./src/client/main.js'],
+  },
+  mode: 'production',
+  output: {
+    filename: 'scripts/[name]-bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          {
+            loader: MiniCSSExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'imgs/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {},
+        },
+      },
+    ],
+  },
+  plugins: [
+    new OptimizeCssAssetsPlugin(),
+    new MiniCSSExtractPlugin({
+      filename: 'styles/[name].css',
+    }),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
