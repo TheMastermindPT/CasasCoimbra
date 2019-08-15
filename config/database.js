28bcef207d62949bc53530c84e79698b2a083dbb
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2019-08-15 23:36:33 +0100

    db connection configs

--- config/database.js
+++ config/database.js
@@ -1,20 +1,35 @@
 const Sequelize = require('sequelize');
 
+let sequelize;
+
 // Option 1: Passing parameters separately
-const sequelize = new Sequelize('casascoimbra', 'root', 'root', {
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(
+    'postgres://xkuhxnxspuahmv:9f93069def35df5237f6aece9c0c2e4b073a1f20d967420c2723f1fc1c0a390f@ec2-54-247-85-251.eu-west-1.compute.amazonaws.com:5432/d49agogrvjeoo2'
+  );
+  console.log('production DB');
+} else {
+  sequelize = new Sequelize('casascoimbra', 'root', 'root', {
     host: 'localhost',
     dialect: 'mysql',
     port: 8889
+  });
+  console.log('Local DB');
+}
+
+// MODELS
+const { User, Casa, Divisao, Foto } = require('../models');
+
+// Connect to DB
+sequelize.authenticate().then(() => {
+  console.log('Connected to database');
 });
 
-//MODELS
-const User = require('../models').User;
-const Casa = require('../models').Casa;
-const Divisao = require('../models').Divisao;
-const Foto = require('../models').Foto;
-
-
-//Connect to DB
-sequelize.authenticate().then(() => { console.log('Connected to database'); });
-
-module.exports = { sequelize: sequelize, User: User, Casa: Casa, Foto: Foto, Divisao: Divisao }
+module.exports = {
+  sequelize,
+  User,
+  Casa,
+  Foto,
+  Divisao
+};
