9640ac980a025b67e2cae185e76799e704d2a419
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2019-08-15 22:05:22 +0100

    procfile

--- src/server/express.js
+++ src/server/express.js
@@ -3,7 +3,7 @@
 // MODULES
 // require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const isProd = process.env.NODE_ENV === 'production' || null;
 const express = require('express');
 const path = require('path');
