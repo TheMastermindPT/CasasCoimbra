5608fb4a9729651689bfd379de2e725694bc12f5
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2019-12-25 18:04:08 +0000

    Done

---
+++ src/client/translate.js
@@ -0,0 +1,24 @@
+const linguJSON = {
+    "languages": [
+        {
+            "lang_name": "English",
+            "lang_code": "en",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "PortuguÊs",
+            "lang_code": "pt",
+            "url_pattern": "?"
+        }
+    ],
+    "translated_segments": [
+        {
+            "source": "Ready to go global?",
+            "target": "¿Está listo para el mundo?"
+        },
+        {
+            "source": "get found by search engines and visitors in other languages",
+            "target": "sea encontrado por los motores de búsqueda en otros idiomas"
+        },
+    ]
+};
