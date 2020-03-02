6bbf4ac94e6c9576075d584637a4da2e6eee6f0c
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2020-03-02 19:49:34 +0000

    env

--- config/database.js
+++ config/database.js
@@ -6,7 +6,8 @@ if (!global.hasOwnProperty('db')) {
   // Option 1: Passing parameters separately
   if (process.env.DATABASE_URL) {
     sequelize = new Sequelize(    
-    "postgres://hpzssqxlftvmow:5deb4fab2c75c9b6f3291d14597fbb307b7f24b2230e41f2f8430d08b5b168ba@ec2-46-137-84-173.eu-west-1.compute.amazonaws.com:5432/d6pnii2gpt1ro1");
+      process.env.DATABASE_URL
+    )
     console.log('production DB');
   } else {
     sequelize = new Sequelize('casascoimbra', 'root', 'root', {
