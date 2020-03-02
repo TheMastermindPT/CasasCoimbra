702ab2561821f60eccf253fe8451ba6a80c7464a
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2020-03-02 19:56:45 +0000

    final

--- src/server/express.js
+++ src/server/express.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/order */
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const isProd = process.env.NODE_ENV === 'production' || null;
 const express = require('express');
 const path = require('path');
