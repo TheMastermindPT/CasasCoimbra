60a60d443196c8fc300a246bcdacc58f31dbd5fb
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2020-03-02 20:40:34 +0000

    jawsmysql

--- config/database.js
+++ config/database.js
@@ -5,9 +5,11 @@ if (!global.hasOwnProperty('db')) {
   let sequelize;
   // Option 1: Passing parameters separately
   if (process.env.DATABASE_URL) {
-    sequelize = new Sequelize(    
-      process.env.DATABASE_URL
-    )
+    sequelize = new Sequelize('trqxjv9cvk44lyum', 'y9fmh98miynewqww', 'yd4fzsqjdz25kbmd', {
+      host: 'cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+      dialect: 'mysql',
+      port : 3306
+    })
     console.log('production DB');
   } else {
     sequelize = new Sequelize('casascoimbra', 'root', 'root', {
